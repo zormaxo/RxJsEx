e3a24cf46417bbd32cb67158222b7580e03d3c1f
Zor Max <okocadere@gmail.com>
2021-10-21 11:23:19 +0300

    from - create stream from an array

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
-import { fromEvent, of } from "rxjs";
+import { fromEvent, of, from } from "rxjs";
 import { map, scan } from "rxjs/operators";
 
-//FROMEVENT - MAKE SUBSCRIPTIONS FROM EVENTS-----------------------------------------------
-fromEvent(document, "click").pipe(
-    scan(count => count + 1, 0)  //similar to reduce in js
-).subscribe(count => console.log(`Clicked ${count} times`));
+//FROM - CREATE STREAM FROM AN ARRAY-----------------------------------------------
+const arraySource = from([1, 2, 3, 4, 5])
+arraySource.subscribe(val => console.log(val));
+
+of(1, 2, 3).subscribe(data => console.log(data));
+
+// //FROMEVENT - MAKE SUBSCRIPTIONS FROM EVENTS-----------------------------------------------
+// fromEvent(document, "click").pipe(
+//     scan(count => count + 1, 0)  //similar to reduce in js
+// ).subscribe(count => console.log(`Clicked ${count} times`));
 
 
 // //OF - CREATE OBSERVABLES FROM NUMBERS-----------------------------------------------
