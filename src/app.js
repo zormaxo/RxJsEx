dca1515b884698ae9fbc7ae702214bfdc9cc3240
Zor Max <okocadere@gmail.com>
2021-10-21 12:31:51 +0300

    observable - create own observables

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-import { fromEvent, of, from } from "rxjs";
+import { fromEvent, of, from, Observable } from "rxjs";
 import { map, scan } from "rxjs/operators";
 
-//FROM - CREATE STREAM FROM AN ARRAY-----------------------------------------------
-const arraySource = from([1, 2, 3, 4, 5])
-arraySource.subscribe(val => console.log(val));
+//OBSERVABLE - CREATE OWN OBSERVABLES-----------------------------------------------
+const observable = new Observable(subscriber => {
+    subscriber.next(1);
+    subscriber.next(2);
+    throw "This is an error";
+    subscriber.next(3);  //of(1,2,3)
+    setInterval(() => {
+        subscriber.next(4);
+        subscriber.complete();
+    }, 1000)
+});
 
-of(1, 2, 3).subscribe(data => console.log(data));
+observable.subscribe({
+    next: val => console.log(val),
+    error: err => console.log(err),
+    complete: () => console.log("completed")
+});
+
+// //FROM - CREATE STREAM FROM AN ARRAY-----------------------------------------------
+// const arraySource = from([1, 2, 3, 4, 5])
+// arraySource.subscribe(val => console.log(val));
+
+// of(1, 2, 3).subscribe(data => console.log(data));
 
 // //FROMEVENT - MAKE SUBSCRIPTIONS FROM EVENTS-----------------------------------------------
 // fromEvent(document, "click").pipe(
