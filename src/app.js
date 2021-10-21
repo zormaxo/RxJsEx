0e93f2c755b42e8b28589a35f872add4fe4eab93
Zor Max <okocadere@gmail.com>
2021-10-21 11:16:41 +0300

    fromEvent - make subscriptions from events

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
-import { of } from "rxjs";
-import { map } from "rxjs/operators";
+import { fromEvent, of } from "rxjs";
+import { map, scan } from "rxjs/operators";
 
-//of - create observables from numbers
-of(1, 2, 3).pipe(
-    map(x => x * x)
-).subscribe(data => console.log(data));
+//FROMEVENT - MAKE SUBSCRIPTIONS FROM EVENTS-----------------------------------------------
+fromEvent(document, "click").pipe(
+    scan(count => count + 1, 0)  //similar to reduce in js
+).subscribe(count => console.log(`Clicked ${count} times`));
+
+
+// //OF - CREATE OBSERVABLES FROM NUMBERS-----------------------------------------------
+// of(1, 2, 3).pipe(
+//     map(x => x * x)
+// ).subscribe(data => console.log(data));
