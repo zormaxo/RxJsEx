6c498582ef427da54ef47637549f612526d9d77e
Zor Max <okocadere@gmail.com>
2021-12-02 20:52:55 +0300

    Revert "remove lines in webpack"
    This reverts commit bc2104999e8c155eec2a2a352d062ecf856a1855.

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,16 @@ module.exports = {
     "./src/13-debounceTime.ts",
     "./src/14-catchError.ts",
   ],
-  devtool: 'inline-source-map', //There were warnings on the console so I added this to prevent getting warnings. I don't know what this is excatly
-  module: {},
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
