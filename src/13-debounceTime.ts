0f8b810a378b0bd96f387739242ebfb53774d785
zormax <zormax052@gmail.com>
2021-12-11 14:25:48 +0300

    some changes

--- src/13-debounceTime.ts
+++ src/13-debounceTime.ts
@@ -1,4 +1,4 @@
-/* import { fromEvent, of, from, Observable, Observer, Subject, Subscriber, interval, timer, range, merge, concat } from "rxjs";
+import { fromEvent, of, from, Observable, Observer, Subject, Subscriber, interval, timer, range, merge, concat } from "rxjs";
 import { map, scan, throttleTime, debounceTime, distinctUntilChanged, takeUntil, take, pluck, delay, mergeMap, switchMap, exhaustMap, concatMap } from "rxjs/operators";
 
 console.log("DEBOUNCETIME");
@@ -11,6 +11,11 @@ const keyup$ = fromEvent(searchBox, 'keyup');
 keyup$.pipe(
   map((i: any) => i.currentTarget.value),
   // throttleTime(1500),
-  debounceTime(1500),               //prevents multiple click
+  debounceTime(1000),               //prevents multiple click
   distinctUntilChanged()
-).subscribe(console.log); */
+).subscribe(console.log);
+
+const clicks = fromEvent(document, 'click');
+const result = clicks.pipe( throttleTime(1000));
+result.subscribe(x => console.log(x));
+
